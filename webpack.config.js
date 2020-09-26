247bd660ea1499fde38e39ae98d3688a1dadc950
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-09-27 02:26:04 +0530

    add basic automation related files

---
+++ webpack.config.js
@@ -0,0 +1,116 @@
+const path = require( 'path' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+const MiniCSSExtractPlugin = require( 'mini-css-extract-plugin' );
+const ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
+const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
+const WebpackRTLPlugin = require( 'webpack-rtl-plugin' );
+const wpPot = require( 'wp-pot' );
+
+const inProduction = ( 'production' === process.env.NODE_ENV );
+const mode = inProduction ? 'production' : 'development';
+
+const config = {
+	mode,
+
+	entry: {
+		'plausible-admin': [ './assets/src/css/admin/main.scss' ],
+	},
+	output: {
+		path: path.join( __dirname, './assets/dist/' ),
+		filename: 'js/[name].js',
+	},
+	devtool: ! inProduction ? 'source-map' : '',
+	module: {
+		rules: [
+
+			// Use Babel to compile JS.
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+
+			// Create RTL styles.
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+			},
+
+			// SASS to CSS.
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCSSExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true,
+							outputStyle: ( inProduction ? 'compressed' : 'nested' ),
+						},
+					} ],
+			},
+
+			// Image files.
+			{
+				test: /\.(png|jpe?g|gif|svg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'images/[name].[ext]',
+							publicPath: '../',
+						},
+					},
+				],
+			},
+		],
+	},
+
+	// Plugins. Gotta have em'.
+	plugins: [
+
+		// Removes the "dist" folder before building.
+		new CleanWebpackPlugin( [ 'assets/dist' ] ),
+
+		new MiniCSSExtractPlugin( {
+			filename: 'css/[name].css',
+		} ),
+
+		new CopyWebpackPlugin( [ { from: 'assets/src/images', to: 'images' } ] ),
+
+	],
+};
+
+if ( inProduction ) {
+	// Create RTL css.
+	config.plugins.push( new WebpackRTLPlugin( {
+		suffix: '-rtl',
+		minify: true,
+	} ) );
+
+	// Minify images.
+	// Must go after CopyWebpackPlugin above: https://github.com/Klathmon/imagemin-webpack-plugin#example-usage
+	config.plugins.push( new ImageminPlugin( { test: /\.(jpe?g|png|gif|svg)$/i } ) );
+
+	// POT file.
+	wpPot( {
+		package: 'Plausible Analytics',
+		domain: 'plausible-analytics',
+		destFile: 'languages/plausible-analytics.pot',
+		relativeTo: './',
+		src: [ './**/*.php', '!./includes/libraries/**/*', '!./vendor/**/*' ],
+		bugReport: 'https://github.com/mehul0810/plausible-analytics-wp/issues/new',
+		team: 'Plausible Analytics Team <hello@plausible.io>',
+	} );
+}
+
+module.exports = config;
