5e90e8c6da382be89bb418d675d065f803350952
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-09-26 18:50:43 +0530

    add plugin main file

---
+++ plausible-analytics.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Plugin Name: Plausible Analytics
+ * Plugin URI: https://plausible.io
+ * Description: Simple and privacy-friendly alternative to Google Analytics.
+ * Author: PlausibleHQ
+ * Author URI: https://plausible.io
+ * Version: 1.0.0
+ * Text Domain: plausible-analytics
+ * Domain Path: /languages
+ *
+ * Plausible Analytics is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Plausible Analytics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Plausible Analytics. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * A Tribute to Open Source:
+ *
+ * "Open source software is software that can be freely used, changed, and shared (in modified or unmodified form) by
+ * anyone. Open source software is made by many people, and distributed under licenses that comply with the Open Source
+ * Definition."
+ *
+ * -- The Open Source Initiative
+ *
+ * Plausible Analytics is a tribute to the spirit and philosophy of Open Source. We at PlausibleHQ gladly embrace the Open Source
+ * philosophy both in how Plausible Analytics itself was developed, and how we hope to see others build more from our code base.
+ *
+ * Plausible Analytics would not have been possible without the tireless efforts of WordPress and the surrounding Open Source projects
+ * and their talented developers. Thank you all for your contribution to WordPress.
+ *
+ * - The Plausible Analytics Team
+ */
+
+namespace Plausible\Analytics\WP;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once __DIR__ . '/config/constants.php';
+
+// Automatically loads files used throughout the plugin.
+require_once PLAUSIBLE_ANALYTICS_PLUGIN_DIR . 'vendor/autoload.php';
+
+// Initialize the plugin.
+$plugin = new Plugin();
+$plugin->register();
