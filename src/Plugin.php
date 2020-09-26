5fba093329760d231be140f4652c0b36131f376a
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-09-26 18:51:07 +0530

    add plugin processing file for namespace

---
+++ src/Plugin.php
@@ -0,0 +1,88 @@
+<?php
+namespace Plausible\Analytics\WP;
+
+use Plausible\Analytics\WP\Admin;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Loads and registers plugin functionality through WordPress hooks.
+ *
+ * @since 1.0.0
+ */
+final class Plugin {
+
+	/**
+	 * Registers functionality with WordPress hooks.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function register() {
+		// Handle plugin activation and deactivation.
+		register_activation_hook( PLAUSIBLE_ANALYTICS_PLUGIN_FILE, [ $this, 'activate' ] );
+		register_deactivation_hook( PLAUSIBLE_ANALYTICS_PLUGIN_FILE, [ $this, 'deactivate' ] );
+
+		// Register services used throughout the plugin.
+		add_action( 'plugins_loaded', [ $this, 'register_services' ] );
+
+		// Load text domain.
+		add_action( 'init', [ $this, 'load_plugin_textdomain' ] );
+	}
+
+	/**
+	 * Registers the individual services of the plugin.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function register_services() {
+		new Admin\Settings();
+	}
+
+	/**
+	 * Loads the plugin's translated strings.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function load_plugin_textdomain() {
+		load_plugin_textdomain(
+			'plausible-analytics',
+			false,
+			dirname( plugin_basename( PLAUSIBLE_ANALYTICS_PLUGIN_FILE ) ) . '/languages/'
+		);
+	}
+
+	/**
+	 * Handles activation procedures during installation and updates.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @param bool $network_wide Optional. Whether the plugin is being enabled on
+	 *                           all network sites or a single site. Default false.
+	 *
+	 * @return void
+	 */
+	public function activate( $network_wide = false ) {}
+
+	/**
+	 * Handles deactivation procedures.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function deactivate() {}
+}
