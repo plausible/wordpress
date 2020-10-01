cd89299e091ba639b774b80316e39326863b2211
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-10-01 23:33:38 +0530

    add frontend actions file

---
+++ src/Includes/Actions.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Plausible Analytics | Actions.
+ *
+ * @since 1.0.0
+ *
+ * @package    WordPress
+ * @subpackage Plausible Analytics
+ */
+
+namespace Plausible\Analytics\WP\Includes;
+
+use Plausible\Analytics\WP\Includes\Helpers;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Actions {
+
+	/**
+	 * Constructor.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		add_action( 'wp_enqueue_scripts', [ $this, 'register_assets' ] );
+	}
+
+	/**
+	 * Register Assets.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function register_assets() {
+		$settings = Helpers::get_settings();
+
+		// Bailout, if `administrator` user role accessing frontend.
+		if ( 'false' === $settings['track_administrator'] && current_user_can( 'manage_options' ) ) {
+			return;
+		}
+
+		wp_enqueue_script( 'plausible-analytics', Helpers::get_analytics_url(), '', PLAUSIBLE_ANALYTICS_VERSION );
+	}
+}
