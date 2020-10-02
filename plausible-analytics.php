527c8e593412e82cbcb1aa46cb8414a39de5361f
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-10-02 22:33:44 +0530

    update license details to mit

--- plausible-analytics.php
+++ plausible-analytics.php
@@ -9,19 +9,6 @@
  * Text Domain: plausible-analytics
  * Domain Path: /languages
  *
- * Plausible Analytics is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * Plausible Analytics is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Plausible Analytics. If not, see <https://www.gnu.org/licenses/>.
- *
  * A Tribute to Open Source:
  *
  * "Open source software is software that can be freely used, changed, and shared (in modified or unmodified form) by
