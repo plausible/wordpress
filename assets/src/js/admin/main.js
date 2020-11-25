3c9700f6ed30f53bc6b68a23678a29a118ddf110
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-11-26 01:58:33 +0530

    improve toggling custom domain and self hosted

--- assets/src/js/admin/main.js
+++ assets/src/js/admin/main.js
@@ -1,25 +1,39 @@
 document.addEventListener( 'DOMContentLoaded', () => {
 	const saveSettings = document.getElementById( 'plausible-analytics-save-btn' );
+	const formElement = document.getElementById( 'plausible-analytics-settings-form' );
+
+	// Bailout, if `formElement` doesn't exist.
+	if ( null === formElement ) {
+		return;
+	}
+
+	const customDomainElement = formElement.querySelector( 'input[name="plausible_analytics_settings[custom_domain]"]' );
+	const selfHostedAnalyticsElement = formElement.querySelector( 'input[name="plausible_analytics_settings[is_self_hosted_analytics]"]' );
+
+	customDomainElement.addEventListener( 'change', ( e ) => {
+		if ( e.target.checked ) {
+			selfHostedAnalyticsElement.removeAttribute( 'checked' );
+			selfHostedAnalyticsElement.checked ?? selfHostedAnalyticsElement.click();
+		}
+	} );
+
+	selfHostedAnalyticsElement.addEventListener( 'change', ( e ) => {
+		if ( e.target.checked ) {
+			customDomainElement.removeAttribute( 'checked' );
+			customDomainElement.checked ?? customDomainElement.click();
+		}
+	} );
 
 	saveSettings.addEventListener( 'click', ( e ) => {
 		e.preventDefault();
-
-		const formElement = document.getElementById( 'plausible-analytics-settings-form' );
-
-		// Bailout, if `formElement` doesn't exist.
-		if ( null === formElement ) {
-			return;
-		}
 
 		const formData = new FormData();
 		const spinner = formElement.querySelector( '.plausible-analytics-spinner' );
 		const domainName = formElement.querySelector( 'input[name="plausible_analytics_settings[domain_name]"]' ).value;
 		const customDomainPrefix = formElement.querySelector( 'input[name="plausible_analytics_settings[custom_domain_prefix]"]' ).value;
-		const customDomainElement = formElement.querySelector( 'input[name="plausible_analytics_settings[custom_domain]"]:checked' );
-		const isCustomDomain = null !== customDomainElement ? parseInt( customDomainElement.value ) : 0;
+		const isCustomDomain = null !== customDomainElement ? customDomainElement.checked : false;
 		const selfHostedDomain = formElement.querySelector( 'input[name="plausible_analytics_settings[self_hosted_domain]"]' ).value;
-		const selfHostedAnalyticsElement = formElement.querySelector( 'input[name="plausible_analytics_settings[is_self_hosted_analytics]"]:checked' );
-		const isSelfHostedAnalytics = null !== selfHostedAnalyticsElement ? parseInt( selfHostedAnalyticsElement.value ) : 0;
+		const isSelfHostedAnalytics = null !== selfHostedAnalyticsElement ? selfHostedAnalyticsElement.checked : false;
 		const trackAdminElement = formElement.querySelector( 'input[name="plausible_analytics_settings[track_administrator]"]:checked' );
 		const isTrackAdmin = null !== trackAdminElement ? parseInt( trackAdminElement.value ) : 0;
 		const roadBlock = null !== formElement.querySelector( '.plausible-analytics-admin-settings-roadblock' ) ? document.querySelector( '.plausible-analytics-admin-settings-roadblock' ).value : '';
@@ -30,9 +44,9 @@ document.addEventListener( 'DOMContentLoaded', () => {
 		formData.append( 'action', 'plausible_analytics_save_admin_settings' );
 		formData.append( 'roadblock', roadBlock );
 		formData.append( 'domain_name', domainName );
-		formData.append( 'custom_domain', isCustomDomain === 1 );
+		formData.append( 'custom_domain', isCustomDomain === true );
 		formData.append( 'custom_domain_prefix', customDomainPrefix );
-		formData.append( 'is_self_hosted_analytics', isSelfHostedAnalytics === 1 );
+		formData.append( 'is_self_hosted_analytics', isSelfHostedAnalytics === true );
 		formData.append( 'self_hosted_domain', selfHostedDomain );
 		formData.append( 'track_administrator', isTrackAdmin === 1 );
 
