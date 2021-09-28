fddb3926e1fa3a91d78b797097e063bd2c619870
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-09-29 00:13:05 +0530

    add upgrade routine for settings

--- src/Admin/Upgrades.php
+++ src/Admin/Upgrades.php
@@ -10,6 +10,8 @@
 
 namespace Plausible\Analytics\WP\Admin;
 
+use Plausible\Analytics\WP\Includes\Helpers;
+
 // Bailout, if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -30,7 +32,45 @@ class Upgrades {
 	 * @return void
 	 */
 	public function __construct() {
+		add_action( 'init', [ $this, 'register_routines' ] );
+	}
 
+	/**
+	 * Register routines for upgrades.
+	 *
+	 * This is intended for automatic upgrade routines having less resource intensive tasks.
+	 *
+	 * @since  1.3.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function register_routines() {
+		// Upgrade to version 1.3.0.
+		if ( version_compare( PLAUSIBLE_ANALYTICS_VERSION, '1.3.0', '<' ) ) {
+			$this->upgrade_to_130();
+		}
+
+		// Add required upgrade routines for future versions here.
+	}
+
+	public function upgrade_to_130() {
+		$old_settings = Helpers::get_settings();
+		$new_settings = $old_settings;
+
+		$new_settings['is_custom_domain'] = $old_settings['custom_domain'];
+		$new_settings['custom_domain']    = "{$old_settings['custom_domain_prefix']}.{$old_settings['domain_name']}";
+		$new_settings['is_shared_link']   = $old_settings['embed_analytics'];
+
+		if ( $old_settings['track_administrator'] ) {
+			$new_settings['track_analytics'] = [ 'administrator' ];
+		}
+
+		// For self hosted plausible analytics.
+		$new_settings['is_self_hosted_plausible_analytics'] = $old_settings['is_self_hosted_analytics'];
+
+		// Update the new settings.
+		update_option( 'plausible_analytics_settings', $new_settings );
 	}
 }
 
