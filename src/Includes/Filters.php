6cb85b3e9bfc725172bfdc4da5f3d4ca69a2fefe
dserv-nh <54544490+dserv-nh@users.noreply.github.com>
2022-03-02 12:53:48 +0000

    feat: add support for excluding pages

--- src/Includes/Filters.php
+++ src/Includes/Filters.php
@@ -37,7 +37,7 @@ class Filters {
 	 * @since  1.0.0
 	 * @access public
 	 *
-	 * @return mixed
+	 * @return string
 	 */
 	public function add_plausible_attributes( $tag, $handle ) {
 		// Bailout, if not `Plausible Analytics` script.
@@ -45,10 +45,18 @@ class Filters {
 			return $tag;
 		}
 
-		$settings    = Helpers::get_settings();
-		$api_url     = Helpers::get_data_api_url();
-		$domain_name = $settings['domain_name'];
+		$settings       = Helpers::get_settings();
+		$api_url        = Helpers::get_data_api_url();
+		$domain_name    = $settings['domain_name'];
 
-		return str_replace( ' src', " async defer data-domain='{$domain_name}' data-api='{$api_url}' src", $tag );
+		$params = "async defer data-domain='{$domain_name}' data-api='{$api_url}'";
+
+		// Triggered when exclude pages is enabled.
+		if ( ! empty( $settings['is_exclude_pages'] ) && $settings['is_exclude_pages'] ) {
+			$excluded_pages = $settings['excluded_pages'];
+			$params .= " data-exclude='{$excluded_pages}'";
+		}
+
+		return str_replace( ' src', " {$params} src", $tag );
 	}
 }
