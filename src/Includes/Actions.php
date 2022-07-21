42072c04450b1225b9cd21f27c7b0fab1a0a8e56
Joost de Valk <joost@altha.nl>
2022-07-21 10:07:24 +0200

    Prevent frontend notice on non-singular pages
    Since this code uses `$post->post_name`, we should only run the code when `is_singular` is true.

--- src/Includes/Actions.php
+++ src/Includes/Actions.php
@@ -90,7 +90,7 @@ class Actions {
 		];
 
 		// Add link to individual page stats.
-		if ( ! is_admin() ) {
+		if ( is_singular() ) {
 			global $post;
 			$args[] = [
 				'id'     => 'view-page-analytics',
