4fba871843a2ee857362c72925ac8ed00bed5d09
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2023-05-23 15:39:18 +0000

    Added: plausible_load_js_in_footer filter.

--- src/Includes/Actions.php
+++ src/Includes/Actions.php
@@ -58,7 +58,7 @@ class Actions {
 		}
 
 		$version = ! empty( $settings['proxy_enabled'][0] ) ? filemtime( Helpers::get_js_path() ) : PLAUSIBLE_ANALYTICS_VERSION;
-		wp_enqueue_script( 'plausible-analytics', Helpers::get_js_url( true ), '', $version );
+		wp_enqueue_script( 'plausible-analytics', Helpers::get_js_url( true ), '', $version, apply_filters( 'plausible_load_js_in_footer', false ) );
 
 		// Goal tracking inline script (Don't disable this as it is required by 404).
 		wp_add_inline_script( 'plausible-analytics', 'window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }' );
