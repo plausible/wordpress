2b83f1ad53a3d7bbc81ad8981cfcc1ce1f001380
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2024-01-15 11:35:09 +0100

    Fixed: added correct error icon.

--- src/Admin/Settings/API.php
+++ src/Admin/Settings/API.php
@@ -66,10 +66,11 @@ class API {
 										</svg>
 									</div>
 									<div id="icon-error" class="flex-shrink-0 hidden">
-										<svg class="h-9 w-6 text-red-400" xmlns="http://www.w3.org/2000/svg"
-											 viewBox="0 0 593 593" stroke="currentColor">
-											<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" fill="#FF4136"
-												  d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/>
+										<svg class="h-8 w-6 text-red-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+											 stroke-width="1.5"
+											 stroke="currentColor">
+											<path stroke-linecap="round" stroke-linejoin="round"
+												  d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
 										</svg>
 									</div>
 									<div class="ml-3 w-0 flex-1 pt-0.5">
