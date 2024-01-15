a1c9657877343e3aff812c8031eca3fcc40db34b
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2024-01-15 11:36:51 +0100

    Changed stroke-width to match with success icon.

--- src/Admin/Settings/API.php
+++ src/Admin/Settings/API.php
@@ -67,7 +67,7 @@ class API {
 									</div>
 									<div id="icon-error" class="flex-shrink-0 hidden">
 										<svg class="h-8 w-6 text-red-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-											 stroke-width="1.5"
+											 stroke-width="2"
 											 stroke="currentColor">
 											<path stroke-linecap="round" stroke-linejoin="round"
 												  d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
