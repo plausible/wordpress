82c9a0eec5b9a10dd222f6d257491e544aa83351
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2024-01-23 10:29:20 +0100

    (De-)activation procedures are no longer required.

--- src/Plugin.php
+++ src/Plugin.php
@@ -23,10 +23,6 @@ final class Plugin {
 	 * @return void
 	 */
 	public function register() {
-		// Handle plugin activation and deactivation.
-		register_activation_hook( PLAUSIBLE_ANALYTICS_PLUGIN_FILE, [ $this, 'activate' ] );
-		register_deactivation_hook( PLAUSIBLE_ANALYTICS_PLUGIN_FILE, [ $this, 'deactivate' ] );
-
 		// Register services used throughout the plugin. (WP Rocket runs at priority 10)
 		add_action( 'plugins_loaded', [ $this, 'register_services' ], 9 );
 
@@ -69,39 +65,5 @@ final class Plugin {
 			false,
 			dirname( plugin_basename( PLAUSIBLE_ANALYTICS_PLUGIN_FILE ) ) . '/languages/'
 		);
-	}
-
-	/**
-	 * Handles activation procedures during installation and updates.
-	 * @since  1.0.0
-	 * @access public
-	 *
-	 * @param bool $network_wide Optional. Whether the plugin is being enabled on
-	 *                           all network sites or a single site. Default false.
-	 *
-	 * @return void
-	 */
-	public function activate( $network_wide = false ) {
-		$is_default_settings_saved = get_option( 'plausible_analytics_is_default_settings_saved', false );
-
-		if ( ! $is_default_settings_saved ) {
-			$domain_name      = Helpers::get_domain();
-			$default_settings = [
-				'domain_name'        => $domain_name,
-				'tracked_user_roles' => [],
-			];
-
-			update_option( 'plausible_analytics_settings', $default_settings );
-			update_option( 'plausible_analytics_is_default_settings_saved', true );
-		}
-	}
-
-	/**
-	 * Handles deactivation procedures.
-	 * @since  1.0.0
-	 * @access public
-	 * @return void
-	 */
-	public function deactivate() {
 	}
 }
