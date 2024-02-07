aae26054d9e7ab562228d659d244f025d9cb6857
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2024-02-07 19:48:26 +0100

    2.0.5

--- plausible-analytics.php
+++ plausible-analytics.php
@@ -5,7 +5,7 @@
  * Description: Simple and privacy-friendly alternative to Google Analytics.
  * Author: Plausible.io
  * Author URI: https://plausible.io
- * Version: 2.0.4
+ * Version: 2.0.5
  * Text Domain: plausible-analytics
  * Domain Path: /languages
  */
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Define plugin version here for convenience.
-define( 'PLAUSIBLE_ANALYTICS_VERSION', '2.0.3' );
+define( 'PLAUSIBLE_ANALYTICS_VERSION', '2.0.5' );
 define( 'PLAUSIBLE_ANALYTICS_PLUGIN_FILE', __FILE__ );
 define( 'PLAUSIBLE_ANALYTICS_PLUGIN_BASENAME', plugin_basename( PLAUSIBLE_ANALYTICS_PLUGIN_FILE ) );
 define( 'PLAUSIBLE_ANALYTICS_PLUGIN_DIR', plugin_dir_path( PLAUSIBLE_ANALYTICS_PLUGIN_FILE ) );
