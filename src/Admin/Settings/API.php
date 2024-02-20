d9b8318f22769c91e0a40f361deb350b3a9c820d
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2024-02-20 15:06:16 +0100

    Improved: don't allow browser autocompletion on text fields.

--- src/Admin/Settings/API.php
+++ src/Admin/Settings/API.php
@@ -595,7 +595,7 @@ class API {
             <div class="mt-1">
                 <input
                         class="block w-full !border-gray-300 !dark:border-gray-700 !rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-900 dark:text-gray-300 py-2 px-3"
-                        id="<?php echo $field[ 'slug' ]; ?>" placeholder="<?php echo $placeholder; ?>" type="text"
+                        id="<?php echo $field[ 'slug' ]; ?>" placeholder="<?php echo $placeholder; ?>" autocomplete="off" type="text"
                         name="<?php echo $field[ 'slug' ]; ?>"
                         value="<?php echo $value; ?>" <?php echo $disabled; ?> />
             </div>
