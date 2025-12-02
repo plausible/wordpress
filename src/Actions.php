93c5c4e9b77bbf41114cbf952ff677858b531917
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-12-02 18:39:50 +0100

    Fixed: call to undefined function get_plugin_data

--- src/Actions.php
+++ src/Actions.php
@@ -40,6 +40,10 @@ class Actions {
 	 * Retrieves the plugin's current version.
 	 */
 	private function get_plugin_version() {
+		if ( ! function_exists( 'get_plugin_data' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/plugin.php';
+		}
+		
 		static $data = null;
 
 		if ( $data === null ) {
