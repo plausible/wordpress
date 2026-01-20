74c56242e1bd70c29b7e8c5113c31b74a9cec088
Daan van den Bergh <18595395+Dan0sz@users.noreply.github.com>
2026-01-20 15:55:55 +0100

    These classes don't contain testable code.

--- src/Capabilities.php
+++ src/Capabilities.php
@@ -4,6 +4,8 @@ namespace Plausible\Analytics\WP;
 
 /**
  * This class behaves like an enum, while we can't really support yet, since WP hasn't dropped support yet for PHP 8.0 and lower.
+ *
+ * @codeCoverageIgnore
  */
 final class Capabilities {
 
--- src/EnhancedMeasurements.php
+++ src/EnhancedMeasurements.php
@@ -4,6 +4,8 @@ namespace Plausible\Analytics\WP;
 
 /**
  * This class behaves like an enum, while we can't really support yet, since WP hasn't dropped support yet for PHP 8.0 and lower.
+ *
+ * @codeCoverageIgnore
  */
 final class EnhancedMeasurements {
 	const FOUR_O_FOUR = '404';
