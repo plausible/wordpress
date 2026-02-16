77ba6c34b9aa75b53869d530b390603f3029a37a
Daan van den Bergh <18595395+Dan0sz@users.noreply.github.com>
2026-02-16 18:23:35 +0100

    Fixed: PHP 7.4 related error in Module::maybe_install_module.

--- src/Admin/Module.php
+++ src/Admin/Module.php
@@ -43,9 +43,12 @@ class Module {
 	 *
 	 */
 	public function maybe_install_module( $old_settings, $settings ) {
-		if ( $settings['proxy_enabled'] === 'on' && $old_settings['proxy_enabled'] !== 'on' ) {
+		$settings_proxy = ( is_array( $settings ) && isset( $settings['proxy_enabled'] ) ) ? $settings['proxy_enabled'] : '';
+		$old_proxy      = ( is_array( $old_settings ) && isset( $old_settings['proxy_enabled'] ) ) ? $old_settings['proxy_enabled'] : '';
+
+		if ( $settings_proxy === 'on' && $old_proxy !== 'on' ) {
 			$this->install();
-		} elseif ( $settings['proxy_enabled'] === '' && $old_settings['proxy_enabled'] === 'on' ) {
+		} elseif ( $settings_proxy === '' && $old_proxy === 'on' ) {
 			$this->uninstall();
 		}
 	}
@@ -196,7 +199,10 @@ class Module {
 		/**
 		 * No need to run this on each update run, or when the proxy is disabled.
 		 */
-		if ( empty( $settings['proxy_enabled'] ) || ( $settings['proxy_enabled'] === 'on' && $old_settings['proxy_enabled'] === 'on' ) ) {
+		$settings_proxy = ( is_array( $settings ) && isset( $settings['proxy_enabled'] ) ) ? $settings['proxy_enabled'] : ( $settings['proxy_enabled'] ?? '' );
+		$old_proxy      = ( is_array( $old_settings ) && isset( $old_settings['proxy_enabled'] ) ) ? $old_settings['proxy_enabled'] : '';
+
+		if ( empty( $settings_proxy ) || ( $settings_proxy === 'on' && $old_proxy === 'on' ) ) {
 			return $settings;
 		}
 
