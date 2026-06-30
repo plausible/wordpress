ccf1b770601175e707a2b49ccdff22cc00e911ca
Daan van den Bergh <18595395+Dan0sz@users.noreply.github.com>
2026-06-30 14:31:25 +0200

    Fixed: attach dismiss event listener to document, because the .notice-dismiss button is added dynamically with JS.

--- assets/src/js/admin/notice.js
+++ assets/src/js/admin/notice.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-	var notice = document.querySelector('.plausible-analytics-multilang-notice .notice-dismiss');
+	document.addEventListener('click', function (e) {
+		if (!e.target.closest('.plausible-analytics-multilang-notice .notice-dismiss')) {
+			return;
+		}
 
-	if (!notice) {
-		return;
-	}
-
-	notice.addEventListener('click', function () {
 		var form = new FormData();
+
 		form.append('action', 'plausible_analytics_dismiss_multilang_notice');
 		form.append('_nonce', plausible_analytics_notice.nonce);
+		
 		fetch(ajaxurl, {method: 'POST', body: form});
 	});
 });
